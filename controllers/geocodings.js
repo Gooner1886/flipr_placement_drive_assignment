662e7832268617f4f29e887db679d96384442b2c
Siddharth Sarma <siddharthgsarma5@gmail.com>
2022-01-15 23:14:09 +0530

    second question done

--- controllers/geocodings.js
+++ controllers/geocodings.js
@@ -2,30 +2,38 @@ let request = require("request");
 
 const getLatitudeLongitude = (req, res) => {
   const addresses = req.body.addresses;
-  /* console.log(addresses); */
+
   const API_KEY = process.env.API_KEY;
-  console.log(typeof API_KEY);
-  const returnedAddresses = [];
+
   let BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+
   addresses.forEach((address) => {
     const each = address.split(" ");
     const formattedAddress = each.join("+");
-    console.log(formattedAddress);
+
     let url = BASE_URL + formattedAddress + "&key=" + API_KEY;
-    console.log(url);
+    const returnedAddresses = [];
+
     request(url, (error, response, body) => {
       if (!error && response.statusCode == 200) {
-        console.log(body);
-        /* returnedAddresses.push({
-            add: address,
-            location: 
-        }) */
+        const parsedBody = JSON.parse(body);
+
+        const latitude = parsedBody.results[0].geometry.location.lat;
+        const longitude = parsedBody.results[0].geometry.location.lng;
+
+        const combinedLocation = [];
+        combinedLocation.push(latitude, longitude);
+
+        const returnedObject = { add: address, location: combinedLocation };
+        returnedAddresses.push(returnedObject);
+        
+        console.log(returnedAddresses);
       } else {
         res.status(500).json({ message: error });
       }
     });
-    res.end()
   });
+  res.end("Answer object in console. Open Dev tools -> Go to Console");
 };
 
 module.exports = { getLatitudeLongitude };
